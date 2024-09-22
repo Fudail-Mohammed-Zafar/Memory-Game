f76b51f208926e99d3c5928563fe51a460355d17
Fudail Mohammed Zafar <fudail.zafar@gmail.com>
2024-09-22 14:22:11 +0530

    Modified the project
    Made the project as time based so increase functionality.

--- script.js
+++ script.js
@@ -1,74 +1,95 @@
-const cards = document.querySelectorAll(".card");
+const cards = document.querySelectorAll(".card"),
+    timeTag = document.querySelector(".time b"),
+    flipsTag = document.querySelector(".flips b"),
+    refreshBtn = document.querySelector(".details button");
 
-let matchCard = 0;
-let cardOne, cardTwo;
+let maxTime = 20;
+let timeLeft = maxTime;
+let flips = 0;
+let matchedCard = 0;
 let disableDeck = false;
+let isPlaying = false;
+let cardOne, cardTwo, timer;
 
-function flipCard(e) {
-    let clickedCard = e.target; //getting user clicked card
-    if (clickedCard !== cardOne && !disableDeck) {
+function initTimer() {
+    if (timeLeft <= 0) {
+        return clearInterval(timer);
+    }
+    timeLeft--;
+    timeTag.innerText = timeLeft;
+}
+
+function flipCard({ target: clickedCard }) {
+    if (!isPlaying) {
+        isPlaying = true;
+        timer = setInterval(initTimer, 1000);
+    }
+    if (clickedCard !== cardOne && !disableDeck && timeLeft > 0) {
+        flips++;
+        flipsTag.innerText = flips;
         clickedCard.classList.add("flip");
         if (!cardOne) {
-            // return the cardOne value to clickedCard
             return cardOne = clickedCard;
         }
         cardTwo = clickedCard;
         disableDeck = true;
-        let cardOneImg = cardOne.querySelector("img").src,
-            cardTwoImg = cardTwo.querySelector("img").src;
+        let cardOneImg = cardOne.querySelector(".back-view img").src,
+            cardTwoImg = cardTwo.querySelector(".back-view img").src;
         matchCards(cardOneImg, cardTwoImg);
     }
 }
 
 function matchCards(img1, img2) {
-    if (img1 == img2) { // if two cards img matched
-        matchCard++; //increment matched value by 1
-        // if matched value is 8 that means user has matched all the cards (8 * 2 = 16 cards)
-        if (matchCard == 8) {
-            setTimeout(() => {
-                return shuffleCard();
-            }, 1000); // calling shuffleCard funtion after 1 sec
+    if (img1 === img2) {
+        matchedCard++;
+        if (matchedCard == 6 && timeLeft > 0) {
+            return clearInterval(timer);
         }
         cardOne.removeEventListener("click", flipCard);
         cardTwo.removeEventListener("click", flipCard);
-        cardOne = cardTwo = ""; //setting both card value to blank
+        cardOne = cardTwo = "";
         return disableDeck = false;
     }
-    // if two card not matched
+
     setTimeout(() => {
-        // adding shake class to both cards after 400ms
         cardOne.classList.add("shake");
         cardTwo.classList.add("shake");
     }, 400);
 
     setTimeout(() => {
-        // adding both shake & flip classes from both the card after 1.2 seconds
         cardOne.classList.remove("shake", "flip");
         cardTwo.classList.remove("shake", "flip");
-        cardOne = cardTwo = ""; //setting card value to blank
+        cardOne = cardTwo = "";
         disableDeck = false;
     }, 1200);
 }
 
 function shuffleCard() {
-    matchedCard = 0;
-    cardOne, cardTwo = "";
-    disableDeck = false;
-    // creating array of 16 items and each item is repeated twice
-    let arr = [1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8];
-    arr.sort(() => Math.random() > 0.5 ? 1 : -1); // sorting array item randomly
+    timeLeft = maxTime;
+    flips = matchedCard = 0;
+    cardOne = cardTwo = "";
+    clearInterval(timer);
+    timeTag.innerText = timeLeft;
+    flipsTag.innerText = flips;
+    disableDeck = isPlaying = false;
 
-    // removing flip class from all cards and passing random image to each card
+    let arr = [1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6];
+    arr.sort(() => Math.random() > 0.5 ? 1 : -1);
+
     cards.forEach((card, index) => {
         card.classList.remove("flip");
-        let imgTag = card.querySelector("img");
-        imgTag.src = `Images/img-${arr[index]}.png`
+        let imgTag = card.querySelector(".back-view img");
+        setTimeout(() => {
+            imgTag.src = `images/img-${arr[index]}.png`;
+        }, 500);
         card.addEventListener("click", flipCard);
     });
 }
 
 shuffleCard();
 
-cards.forEach(card => { // adding click event to all cards
+refreshBtn.addEventListener("click", shuffleCard);
+
+cards.forEach(card => {
     card.addEventListener("click", flipCard);
 });
